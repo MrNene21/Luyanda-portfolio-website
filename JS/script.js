6e9f6a39fa42e53ba3c8c88620c1533cee574800
Luyanda Nene <luyandanene50@gmail.com>
2022-07-07 20:29:54 +0200

    included download feature

--- JS/script.js
+++ JS/script.js
@@ -4,12 +4,4 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-
-    window.onload = function(){
-        //This setTimeout function is meant to emulate page loading.
-          setTimeout(function(){
-            var el = document.getElementById('cont');
-            el.classList.add('loaded');
-          }, 5000)
-        }
 });
