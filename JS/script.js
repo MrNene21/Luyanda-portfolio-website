417cbb826b7531f11b6923b4bfac1e4178f5c50f
Luyanda Nene <luyandanene50@gmail.com>
2022-07-07 18:30:28 +0200

    recovered code

--- JS/script.js
+++ JS/script.js
@@ -4,23 +4,12 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    window.onload = function(){
+        //This setTimeout function is meant to emulate page loading.
+          setTimeout(function(){
+            var el = document.getElementById('cont');
+            el.classList.add('loaded');
+          }, 5000)
+        }
 });
-
-const wait = (delay = 5) =>
-  new Promise(resolve => setTimeout(resolve, delay));
-
-const setVisible = (elementOrSelector, visible) => 
-  (typeof elementOrSelector === 'string'
-    ? document.querySelector(elementOrSelector)
-    : elementOrSelector
-  ).style.display = visible ? 'block' : 'none';
-
-setVisible('../HTML/index.html', false);
-setVisible('../HTML/loading.html', true);
-
-document.addEventListener('DOMContentLoaded', () =>
-  wait(1000).then(() => {
-    setVisible('../HTML/index.html', true);
-    setVisible('../HTML/loading.html', false);
-  }));
-
