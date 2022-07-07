30b708087d261fcf8c86ac1862296dcc36ce75a8
Luyanda Nene <luyandanene50@gmail.com>
2022-07-07 16:41:27 +0200

    busy with pre loader

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,21 @@ $(document).ready(function(){
     });
 });
 
+const wait = (delay = 5) =>
+  new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (elementOrSelector, visible) => 
+  (typeof elementOrSelector === 'string'
+    ? document.querySelector(elementOrSelector)
+    : elementOrSelector
+  ).style.display = visible ? 'block' : 'none';
+
+setVisible('../HTML/index.html', false);
+setVisible('../HTML/loading.html', true);
+
+document.addEventListener('DOMContentLoaded', () =>
+  wait(1000).then(() => {
+    setVisible('../HTML/index.html', true);
+    setVisible('../HTML/loading.html', false);
+  }));
+
